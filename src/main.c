4a25dc89c336ce8f40776178cb638cb20ca57f30
Xiaojun Huang <xhuang@2-F-5.42heilbronn.de>
2024-12-18 18:25:06 +0100

    map init

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junjun <junjun@student.42.fr>              +#+  +:+       +#+        */
+/*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:58:35 by junjun            #+#    #+#             */
-/*   Updated: 2024/12/17 17:50:24 by junjun           ###   ########.fr       */
+/*   Updated: 2024/12/18 18:04:00 by xhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,20 @@ void free_table()
 
 int main(int ac, char **av)
 {
+	int fd;
+	t_map *map;
+	
 	if (ac != 2)
 	{
 		return (0);
 	}
-	
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+	{
+		//read error
+	}
+	map = map_init();
+	read_map(fd, )
 	void *mlx;
 	void *mlx_win;
 
--- src/map_initi.c
+++ src/map_initi.c
@@ -3,14 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   map_initi.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junjun <junjun@student.42.fr>              +#+  +:+       +#+        */
+/*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:58:26 by junjun            #+#    #+#             */
-/*   Updated: 2024/12/17 17:53:17 by junjun           ###   ########.fr       */
+/*   Updated: 2024/12/18 18:24:47 by xhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	map_init(t_map *map);
+{
+	map->width = 0;
+	map->height = 0;
+}
+
+
+int read_map(const int fd, t_map *map)
+{
+	char *line_str;
+	char **line_arr;
+	int i = 0;
+	
+	//malloc how many lines
+	while (line_str = get_next_line(fd))
+	{
+		line_arr = ft_split(line_str, ' ');
+		if (!line_arr)
+		{
+			//error and free
+		}
+		while (line_arr[i++])
+		{
+			map->width++;
+		}
+		free(line_arr);
+		free(line_str);
+		map->height++;
+	}
+	free(line_str);
+	close(fd);
+	return(0);
+}
+
+
+//get value of map
+int  value = ft_atoi(line_arr[i]);
+
+
+
+
+
+
+
+
 
 
 
